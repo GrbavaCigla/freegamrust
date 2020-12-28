72dffb6a4e75c502d62bc5521f5149f9da2d4970
Aleksa Ognjanovic <alexa.ognjanovic@protonmail.com>
2020-12-28 12:40:53 +0100

    Settings

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,63 @@
-use xosd_rs::{Command, HorizontalAlign, Result, VerticalAlign, Xosd};
+use std::error::Error;
+use std::result::Result;
+use xosd_rs::{Command, HorizontalAlign, VerticalAlign, Xosd};
 
-fn main() -> Result<()> {
+trait XosdDrawLines {
+    fn draw_lines(
+        lines: &Vec<&str>,
+        horizontal_offset: i32,
+        vertical_offset: i32,
+        alignment: (VerticalAlign, HorizontalAlign),
+    ) -> xosd_rs::Result<()>;
+}
+
+impl XosdDrawLines for Xosd {
+    fn draw_lines(
+        lines: &Vec<&str>,
+        horizontal_offset: i32,
+        vertical_offset: i32,
+        alignment: (VerticalAlign, HorizontalAlign),
+    ) -> xosd_rs::Result<()> {
+
+        for line in lines.iter() {
+            println!("{}", line);
+        }
+        Ok(())
+    }
+}
+
+
+
+fn decode_alignment(number: u8) -> (VerticalAlign, HorizontalAlign) {
+    let vert = number / 3;
+    let horiz = number % 3;
+
+    let vert = [
+        VerticalAlign::Top,
+        VerticalAlign::Center,
+        VerticalAlign::Bottom,
+    ][vert as usize];
+    let horiz = [
+        HorizontalAlign::Left,
+        HorizontalAlign::Center,
+        HorizontalAlign::Right,
+    ][horiz as usize];
+
+    (vert, horiz)
+}
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let mut settings = config::Config::default();
+
+    settings.merge(config::File::with_name("res/config.toml"))?;
+
+    let horiz_offset = settings.get_int("horizontal-offset")?;
+    let vert_offset = settings.get_int("vertical-offset")?;
+    let alignment = decode_alignment(settings.get_int("alignment")? as u8);
+
     let mut osd = Xosd::new(2)?;
 
-    osd.set_timeout(3)?;
-    osd.set_vertical_align(VerticalAlign::Center)?;
-    osd.set_horizontal_align(HorizontalAlign::Center)?;
-
-    osd.display(0, Command::string("Hello,")?)?;
-    osd.display(1, Command::string("World!")?)?;
-
-    if osd.onscreen()? {
-        osd.wait_until_no_display()?;
-    }
+    osd.draw_lines(&["blaaa", "bla"]);
 
     Ok(())
 }
